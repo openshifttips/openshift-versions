710ffe934300d8ef522c4446f1083f6346ea1aab
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-08-18 13:42:15 +0200

    Use cache
    Signed-off-by: Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 
 mkdir -p content
 pip install -r requirements.txt
+
+# Versions.json and index.html will only be updated if there's a new version, older ones restored from cache
 python ./openshift_versions/cmd/versions.py
+
+# Always copy the resulting file to have a populated web server (even if it's the same)
 cp index.html content/
